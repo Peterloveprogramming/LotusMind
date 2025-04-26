b3b7b819a96ef21056b89fd6312e14f63d15fdcb
Peterloveprogramming <y.peter998@gmail.com>
2025-04-26 09:04:52 +0100

    comit

--- chakareport/dummy_chakarareport_maker_test.go
+++ chakareport/dummy_chakarareport_maker_test.go
@@ -1,1 +1,31 @@
 package chakareport
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateNewDummyChakaraReportMaker(t *testing.T) {
+	maker, err := CreateChakraMakerForTesting(DummyChakaraReportMakerType)
+	require.NoError(t, err)
+	require.NotNil(t, maker)
+	require.Equal(t, maker.GetType(), DummyChakaraReportMakerType)
+}
+
+func TestGenerateReportWithDummyChakaraReportMaker(t *testing.T) {
+	maker, err := CreateChakraMakerForTesting(DummyChakaraReportMakerType)
+	require.NoError(t, err)
+	require.NotNil(t, maker)
+	require.Equal(t, maker.GetType(), DummyChakaraReportMakerType)
+
+	testChakaraInfo := []ChakraInfo{
+		{ChakraName: "Root", ChakraScore: 75, ChakraStatus: "Balanced"},
+		{ChakraName: "Sacral", ChakraScore: 40, ChakraStatus: "Blocked"},
+	}
+	testLanguage := "en"
+
+	reportBytes, err := maker.GenerateChakaraReport(testChakaraInfo, testLanguage)
+	require.NoError(t, err)
+	require.NotNil(t, reportBytes)
+}
--- storage/storage.helper.go
+++ storage/storage.helper.go
@@ -1,8 +1,38 @@
 package storage
 
+import "fmt"
+
 const TimeStampFormat = "200601021504"
 const ReportFolderName = "chakara-report"
 const ReportFileExtension = ".txt"
 
 // 0755 provides standard permissions (rwxr-xr-x).
 const FilePermission = 0755
+
+const LocalStorageMakerType = "LocalStorageMaker"
+const S3StorageMakerType = "S3StorageMaker"
+
+func CreateStorageMakerForTesting(makerType string) (Maker, error) {
+	appUrl := "http://api:8080"
+	prod := "prod"
+	dev := "dev"
+
+	switch makerType {
+	case ChakaraReportMakerType:
+
+		ChakararReportMaker, err := ChakraMaker(prod, appUrl)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create ChakaraReportMaker for testing: %w", err)
+		}
+		return ChakararReportMaker, nil
+	case DummyChakaraReportMakerType:
+		DummyChakararReportMaker, err := ChakraMaker(dev, appUrl)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create DummyChakararReportMaker for testing: %w", err)
+		}
+		return DummyChakararReportMaker, nil
+	default:
+		// Return an error if the provided type string is not recognized
+		return nil, fmt.Errorf("invalid maker type provided for testing: %s", makerType)
+	}
+}
