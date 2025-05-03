8b85d533f1366ae8cfb0799b5465d848d346e972
Peterloveprogramming <y.peter998@gmail.com>
2025-05-03 15:01:48 +0100

    fix email not sending

--- email/send_email_helper.go
+++ email/send_email_helper.go
@@ -2,5 +2,5 @@ package sendemail
 
 const Subject = "🌈 Your Chakra Test Results + 15% Off Just for You!"
 const SubjectFrench = "🌈 Tes Résultats du Test des Chakras + 15% de Réduction Rien que pour Toi !"
-const French = "French"
+const French = "Français"
 const English = "English"
