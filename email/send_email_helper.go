60e5e504f0795ed950d1f3c6951cc2b3ad2a50f1
Peterloveprogramming <y.peter998@gmail.com>
2025-05-23 18:17:51 +0100

    fix discount %

--- email/send_email_helper.go
+++ email/send_email_helper.go
@@ -1,6 +1,6 @@
 package sendemail
 
-const Subject = "🌈 Your Chakra Test Results + 15% Off Just for You!"
-const SubjectFrench = "🌈 Tes Résultats du Test des Chakras + 15% de Réduction Rien que pour Toi !"
+const Subject = "🌈 Your Chakra Test Results + 50% Off Just for You!"
+const SubjectFrench = "🌈 Tes Résultats du Test des Chakras + 50% de Réduction Rien que pour Toi !"
 const French = "Français"
 const English = "English"
