ae8d21201f22da08be39d5d7929e6d315c3e1ba8
Peterloveprogramming <y.peter998@gmail.com>
2025-07-07 17:19:41 +0100

    fix

--- util/config.go
+++ util/config.go
@@ -27,7 +27,7 @@ type Config struct {
 	FrontEndUrlDev         string        `mapstructure:"FRONTEND_URL_DEV"`
 	FrontEndUrlProd        string        `mapstructure:"FRONTEND_URL_PROD"`
 	ApiGateWayEndpoint     string        `mapstructure:"API_GATE_WAY_ENDPOINT"`
-	ApiGateWayApiKey       string        `mapstructure:"API_GATE_WAY_API_KEY`
+	ApiGateWayApiKey       string        `mapstructure:"API_GATE_WAY_API_KEY"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
